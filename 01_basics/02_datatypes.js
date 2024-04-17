ca88d45ab4c910293d2536039a8dc293fedfeff0
Mohitraj0031 <166917404+Mohitraj0031@users.noreply.github.com>
2024-04-17 14:01:54 +0000

    save done

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,17 @@
+"use strict"; // treat all js code as newer version
+
+
+// premetive datatype
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unoque
+
+
+// object
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
