c5ef60542971de6ec5a9f3d01fba759f0d7709b8
Mohitraj0031 <166917404+Mohitraj0031@users.noreply.github.com>
2024-04-18 11:38:01 +0000

    more about conversions

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -24,5 +24,37 @@ let booleanIsLoggedIn = Boolean (isLoggedIn)
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+//console.log(stringNumber);
+//console.log(typeof stringNumber);
+
+// ***************************** Operations ********************
+/* console.log(2+2)
+console.log(2-2)
+console.log(2*2)
+console.log(2/2)
+console.log(2%3)
+*/
+
+//let str1 = " hello "
+
+//let str2 = " Mohit "
+
+//let str3 = str1 + str2
+//console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+// console.log(1 + "2" + 2);
+
+// let x = 3
+// const y = x++;
+// console.log(x);
+// console.log(y);
+
+let a = 3;
+const b = ++a;
+console.log(a, b);
+// console.log(b);
+
