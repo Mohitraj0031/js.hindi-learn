f782ff860515fa1d83d9c9f96704bcc530cc360f
Mohitraj0031 <166917404+Mohitraj0031@users.noreply.github.com>
2024-04-18 11:00:48 +0000

    conversions of datatypes

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,28 @@
+let score = ""
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+
+// "26" => 26
+// "26kg" => NaN (not a number)
+// true => 1; false => 0
+
+let isLoggedIn = "mohit"
+let booleanIsLoggedIn = Boolean (isLoggedIn)
+//console.log(booleanIsLoggedIn);
+//console.log(typeof booleanIsLoggedIn);
+
+// 1=> true; 0 => false
+// "" => false
+// "mohit" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
