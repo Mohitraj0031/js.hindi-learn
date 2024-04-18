470624d9d706b265c4e7789721c4d74342344ec6
Mohitraj0031 <166917404+Mohitraj0031@users.noreply.github.com>
2024-04-18 11:57:31 +0000

    comparisions done

---
+++ 01_basics/04_comparisions.js
@@ -0,0 +1,16 @@
+// Simple comparisions
+// console.log(2 > 1); // output true
+// console.log(0 < 1); // output true
+
+// Comparisons where  js itself change the data-type
+// console.log("2" > 1); // output true
+// console.log("02" > 1); // output true
+
+// In this comparision null is treated as 0
+//console.log(null > 0); // output = false
+//console.log(null == 0); // output = false
+//console.log(null >= 0); // output = true
+
+console.log(undefined > 0); // output = false
+console.log(undefined == 0); // output = false
+console.log(undefined >= 0); // output = false
