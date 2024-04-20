9b5887ed1092012d7b304cec97e77313abf8fc81
Mohitraj0031 <166917404+Mohitraj0031@users.noreply.github.com>
2024-04-20 12:19:16 +0000

    string summary

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -2,4 +2,20 @@ const name = "mohit"
 const repoCount = 45
 
 //console.log(name + repoCount);
-console.log(`hello my name is ${name} and my repo count is ${repoCount}`);
+//console.log(`hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('mohit')
+//console.log(gameName[0])
+//console.log(gameName.__proto__);
+//console.log(gameName.length)
+//console.log(gameName.toUpperCase());
+//console.log(gameName.charAt(4));
+
+//console.log(gameName.indexOf('t'));
+
+const newStringOne = "     This     "
+console.log(newStringOne.trim());
+
+const url = "https://hitesh.com/hitesh%20sharma"
+
+console.log(url.replace('%20', '-'))
