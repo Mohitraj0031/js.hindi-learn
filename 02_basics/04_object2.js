6ae752299905c1ac5eae254ca985d2c5ebfa61ca
Mohitraj0031 <166917404+Mohitraj0031@users.noreply.github.com>
2024-04-26 12:32:17 +0000

    object2

---
+++ 02_basics/04_object2.js
@@ -0,0 +1,33 @@
+//const tindserUser = new Object();
+// same same but different the above one is used for singletone 
+const tinderUser = {}
+
+tinderUser.id = "123"
+tinderUser.name = "Raju"
+tinderUser.isLoggedIn = false
+
+// console.log(tinderUser);
+
+const regularUser = {
+    email : "Raju@google.com",
+    fullname: {
+        userfullname: {
+            firstname: "Raju",
+            lastname: "Rajasthani"
+        }
+    }
+    
+}
+//console.log(regularUser);
+
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "a",4: "b"}
+
+//const obj3 = { obj1, obj2}
+//const obj3 = Object.assign({},obj1,obj2)
+const obj3 = {...obj1,...obj2}
+// console.log(obj3);
+
+console.log(Object.keys(tinderUser))
+console.log(Object.values(tinderUser));
+
