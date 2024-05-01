17766093350f3c046f5c98fd23157a5a1d067015
Mohitraj0031 <166917404+Mohitraj0031@users.noreply.github.com>
2024-05-01 12:52:04 +0000

    functions

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -24,6 +24,35 @@ function addTwoNumbers (number1,number2) {
  }
 
 //  console.log(loginUserMessage(""));
- console.log(loginUserMessage());
+ // console.log(loginUserMessage());
 
+// rest operator (...) also known as spread operatot
+ function calculateCartPrice(val1, val2,...num1) {
+    return num1
+ }
+ // console.log(calculateCartPrice(4,8,12,16,20));
+
+const user ={
+      username: "Raj",
+      price: 1499
+}
+
+function handleObject(anyobject) {
+    console.log(`username is ${anyobject.username} and price is ${anyobject.price}`);
+    
+}
+
+// handleObject(user)
+// handleObject({
+//     username: "sam",
+//     price: 1499
+// })
+
+const myNewArray = [200,400,100,250]
+
+function returnSecondValue (getArray){
+    return getArray[1]
+}
+// console.log(returnSecondValue(myNewArray))
+console.log(returnSecondValue([200,400,500,1000]));
 
